3176d7f1c590976bcfe36cbe5df52f39d0e87de8
Marco Karpf <2marco.karpf2@gmail.com>
2022-09-18 10:21:09 +0200

    add thirdly.fxml for mongo access db, add styling fo content pane gui

--- src/main/java/bbw/km/ThirdlyController.java
+++ src/main/java/bbw/km/ThirdlyController.java
@@ -1,19 +1,18 @@
 package bbw.km;
 
 import bbw.km.dao.DBAccessFactory;
-import bbw.km.dao.JokeDAO;
 import bbw.km.dao.MONGODAO;
 import bbw.km.model.Joke;
 import bbw.km.model.JokeBook;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 import java.io.IOException;
 import java.sql.Date;
-import java.text.SimpleDateFormat;
 
-public class SecondaryController {
+public class ThirdlyController {
 
     @FXML
     private Text lblOut;
@@ -36,22 +35,23 @@ public class SecondaryController {
     }
 
     @FXML
-    private void switchToThirdly() throws Exception {
-        App.setRoot("thirdly");
+    private void switchToSecondary() throws Exception {
+        App.setRoot("secondary");
     }
 
     Joke joke = new Joke();
 
     DBAccessFactory dbAccessFactory = new DBAccessFactory();
-    JokeDAO accdbdao = dbAccessFactory.getDataBaseAccess("MDB");
+    MONGODAO mongodao = (MONGODAO) dbAccessFactory.getDataBaseAccess("MONGO");
+
 
 
     @FXML
     public void listAllJokes() {
-        JokeBook jokeBook = accdbdao.getAllJokes();
+        JokeBook jokeBook = mongodao.getAllJokes();
         lblOut.setText(jokeBook.printJokeBook());
         lblOut.wrappingWidthProperty().set(950);
-        lblOut.setFont(new javafx.scene.text.Font(16));
+        lblOut.setFont(new Font(16));
     }
 
     @FXML
@@ -61,7 +61,7 @@ public class SecondaryController {
         } else {
             try {
                 joke.setUpJoke(Integer.parseInt(txtId.getText()), txtJoke.getText(), Date.valueOf(txtDate.getText()), Integer.parseInt(txtRating.getText()));
-                accdbdao.createJoke(joke);
+                mongodao.createJoke(joke);
                 lblOut.setText("Joke added!");
             } catch (Exception e) {
                 lblOut.setText("Please enter a valid date!");
@@ -76,7 +76,7 @@ public class SecondaryController {
         } else {
             try {
                 joke.setUpJoke(Integer.parseInt(txtId.getText()), txtJoke.getText(), Date.valueOf(txtDate.getText()), Integer.parseInt(txtRating.getText()));
-                accdbdao.updateJoke(joke);
+                mongodao.updateJoke(joke);
                 lblOut.setText("Joke updated!");
             } catch (Exception e) {
                 lblOut.setText("Please enter a valid date!");
@@ -89,8 +89,31 @@ public class SecondaryController {
         if (txtId.getText().isEmpty()) {
             lblOut.setText("Please fill in the ID field!");
         } else {
-            accdbdao.deleteJoke(joke.getId());
+            mongodao.deleteJoke(joke.getId());
             lblOut.setText("Joke deleted!");
         }
     }
 }
+
+
+//        // get the DAO and choose mongo database
+//        mongodao.getAllJokes();
+//
+//        //adding new joke to mongo database
+//        Joke joke3 = new Joke();
+//        try {
+//            joke3.setUpJoke(6, "What did the router say to the doctor? 'It hurts when IP'", new java.util.Date(), 3);
+//            mongodao.createJoke(joke3);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//        //updating joke in mongo database
+//        joke3.setRating(9);
+//        mongodao.updateJoke(joke3);
+//
+//        //deleting joke from mongo database
+//        mongodao.deleteJoke(joke3.getId());
+
+
+
